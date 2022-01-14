23cfbc5700b79f89be18125bc43332cb58441cec
“wafulasam” <wafulasammie@gmail.com>
2022-01-15 00:40:18 +0300

    - add styles (colors)

---
+++ styles/colors.js
@@ -0,0 +1,10 @@
+// colors
+
+export const COLOR_BLACK = "#000000";
+export const COLOR_DARK = "#1D1D1D";
+export const COLOR_LIME = "#DEF5DA";
+export const COLOR_TEXT = "#4D4D4D";
+export const COLOR_GREEN = "#55D187";
+
+export const COLOR_WHITE = "#ffffff";
+export const LIGHT_GRAY = "#e9ebef";
