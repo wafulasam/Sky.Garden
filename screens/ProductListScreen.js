3500a847e1791726f8a048da5f097f63b9ce9032
“wafulasam” <wafulasammie@gmail.com>
2022-01-15 02:21:21 +0300

    - modify screen navigation

--- screens/ProductListScreen.js
+++ screens/ProductListScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import MainLayout from '../layouts/MainLayout';
+import { PRODUCT_DETAIL_SCREEN, CART_SCREEN } from '../navigation/ScreenNames';
 
-export default function ProductListScreen() {
+export default function ProductListScreen({ navigation }) {
   return (
     <MainLayout>
         <View>
             <Text>Product list</Text>
+            <Text onPress={() => navigation.navigate(PRODUCT_DETAIL_SCREEN)}>Details</Text>
+            <Text onPress={() => navigation.navigate(CART_SCREEN)}>Cart</Text>
         </View>
     </MainLayout>
   );
