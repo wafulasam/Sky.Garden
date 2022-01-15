162d8e40b9ba9a6c4c600c02367bc1524065815d
“wafulasam” <wafulasammie@gmail.com>
2022-01-15 13:25:34 +0300

    - add logo and menu

--- components/MobileHeader.js
+++ components/MobileHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
 import { COLOR_BLACK, COLOR_DARK, COLOR_WHITE} from "../styles/colors";
 import { AntDesign} from "@expo/vector-icons";
 
@@ -17,7 +17,10 @@ export default function MobileHeader() {
                 </View>
             </View>
             <View style={styles.header}>
-                <Text style={styles.text}>topbar </Text>
+                <View style={styles.imageWrapper}>
+                    <Image source={require("../assets/images/menu.png")} style={styles.hamburgerMenu} />
+                    <Image source={require("../assets/images/skygarden.png")} style={styles.logo} />
+                </View>
             </View>
         </View>
     );
@@ -36,6 +39,7 @@ const styles = StyleSheet.create({
         width: Dimensions.get("window").width,
         backgroundColor: COLOR_DARK,
         flexDirection: "row",
+        justifyContent: "space-between"
     },
     text: {
         color: COLOR_WHITE,
@@ -43,5 +47,20 @@ const styles = StyleSheet.create({
     },
     textWrapper: {
         flexDirection: "row",
+    },
+    imageWrapper: {
+        flexDirection: "row",
+    },
+    hamburgerMenu: {
+        height: 20,
+        width: 20,
+        marginTop: 15,
+        marginHorizontal: 5,
+    },
+    logo: {
+        height: 30,
+        width: 90,
+        marginVertical: 10,
+        marginHorizontal: 10,
     }
 });
