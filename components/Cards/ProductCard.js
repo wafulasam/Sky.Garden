55a91eb6905d9a5de44fcf8f62012d67420a4f45
“wafulasam” <wafulasammie@gmail.com>
2022-01-15 20:56:57 +0300

    - [feature]: routing;  product list to product details page

--- components/Cards/ProductCard.js
+++ components/Cards/ProductCard.js
@@ -1,25 +1,30 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
-import { BORDER_RADIUS, BORDER_RADIUS_MAX } from "../../styles/variables";
-import { LIGHT_GRAY, PRIMARY_BLUE, COLOR_WHITE } from "../../styles/colors";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { BORDER_RADIUS,  } from "../../styles/variables";
+import { LIGHT_GRAY, COLOR_WHITE } from "../../styles/colors";
 import { vh, vw } from "react-native-css-vh-vw";
-// const width = Dimensions.get("screen").width / 2 - 30;
+import { useNavigation } from "@react-navigation/native";
+import { PRODUCT_DETAIL_SCREEN } from "../../navigation/ScreenNames"
 
 export default function ProductCard({data}) {
   const product = data;
+  const navigation = useNavigation();
   
   return (
     <View style={styles.card}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate(PRODUCT_DETAIL_SCREEN, { data: product })}
+      >
         <Image source={{uri: product.thumbnail}} style={styles.productImage} />
         <Text style={styles.name}>{product.title}</Text>
         <Text style={styles.pricing}>{ product.stock_record_price_currency} {product.stock_record_price_retail}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    // width,
     width: vw(45),
     padding: vw(2.5),
     margin: 10,
@@ -30,8 +35,6 @@ const styles = StyleSheet.create({
   },
   productImage: {
     height: vh(20),
-    // width: "100%",
-    // borderRadius: BORDER_RADIUS,
     margin: 10,
   },
   name: {
--- screens/ProductDetailScreen.js
+++ screens/ProductDetailScreen.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Text, View } from 'react-native';
 import MainLayout from '../layouts/MainLayout';
 
-export default function ProductDetailScreen() {
+export default function ProductDetailScreen({ route }) {
+  const product = route.params.data;
+
   return (
     <MainLayout>
         <View>
             <Text>Product details</Text>
+            <Text>{JSON.stringify(product)}</Text>
         </View>
     </MainLayout>
   );
