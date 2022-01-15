71268746a176c3cdf60bca764155723f39d99365
“wafulasam” <wafulasammie@gmail.com>
2022-01-15 20:45:10 +0300

    [BUG]: - fix flatlist scroll

--- screens/ProductListScreen.js
+++ screens/ProductListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, ImageBackground, FlatList } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, FlatList, Dimensions } from 'react-native';
 import MainLayout from '../layouts/MainLayout';
 import { vh } from "react-native-css-vh-vw";
 import BreadCrumbs from '../components/BreadCrumbs';
@@ -11,7 +11,6 @@ import { listings } from "../data"
 export default function ProductListScreen({ navigation }) {
   return (
     <MainLayout>
-      <View style={styles.container}>
         <ImageBackground
           source={require("../assets/images/bg-1.png")}
           style={styles.backgroundImage}
@@ -20,33 +19,24 @@ export default function ProductListScreen({ navigation }) {
             details="Home / Mobile phones & Tablets"
             activeDetail="Smartphones"
           />
-          <View style={styles.wrapper}>
-              <FlatList
-                ListHeaderComponent={()=> <Text style={styles.heading}>Smartphones</Text>}
-                ListFooterComponent={()=> <Pagination/>}
-                showsVerticalScrollIndicator={false}
-                numColumns={2}
-                data={listings}
-                renderItem={({ item }) => <ProductCard data={item} />}
-                keyExtractor={(item) => item.title}
+            <FlatList
+              ListHeaderComponent={()=> <Text style={styles.heading}>Smartphones</Text>}
+              ListFooterComponent={()=> <Pagination/>}
+              showsVerticalScrollIndicator={false}
+              numColumns={2}
+              data={listings}
+              renderItem={({ item }) => <ProductCard data={item} />}
+              keyExtractor={(item) => item.title}
             />
-          </View>
         </ImageBackground>
-      </View>
     </MainLayout>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   backgroundImage: {
-    height: vh(40),
+    height: vh(80),
     width: "100%",
-  },
-  wrapper: {
-    minHeight: vh(100),
   },
   heading: {
     fontSize: 30,
