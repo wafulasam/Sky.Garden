160223ec3fbf2549d6855b1ca6e57ebbd0d1bede
“wafulasam” <wafulasammie@gmail.com>
2022-01-16 12:51:21 +0300

    [feature]: global data store = useAppStore

--- store/store.js
+++ store/store.js
@@ -2,3 +2,11 @@ import { createStore } from "redux";
 import { rootReducer } from "./reducers";
 
 export const store = createStore(rootReducer);
+
+export const useAppStore = () => {
+    const products = useSelector((state) => state.productsReducer.products).flat(); // flat is used to remove the second bracket.
+ 
+    return {
+       products,
+    }
+ }
